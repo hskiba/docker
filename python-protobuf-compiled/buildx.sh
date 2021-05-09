8d1763071bfa23659deab5338f8525ce3f6b2d9c
Henry Skiba <henry.skiba@frgrisk.com>
2021-05-09 14:24:27 +0800

    Update version to 3.16.0 and add docker buildx script

---
+++ python-protobuf-compiled/buildx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker buildx build \
+       --push \
+       --platform linux/arm64,linux/amd64 \
+       --tag henryskiba/python-protobuf-compiled:latest .
+docker buildx build \
+       --push \
+       --platform linux/arm64,linux/amd64 \
+       --tag henryskiba/python-protobuf-compiled:3.16.0 .
